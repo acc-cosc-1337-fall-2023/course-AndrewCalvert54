95335cd716a3a2d885ca5361a33789a7b7b4fa4e
AndrewCalvert54 <143145146+AndrewCalvert54@users.noreply.github.com>
2023-09-09 00:41:38 +0000

    Homework 2

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -1,5 +1,6 @@
 #include "hwexpressions.h"
 
+
 //example
 int add_numbers(int num1, int num2)
 {
@@ -8,6 +9,13 @@ int add_numbers(int num1, int num2)
 
 //write function code here
 
+double get_sales_tax_amount(double meal_amount)
+{
+	return tax_rate * meal_amount;
+}
 
-
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+	return meal_amount * tip_rate;
+}
 
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,11 +1,37 @@
 ﻿//write include statements
+#include <iostream>
+#include "hwexpressions.h"
 
 
 //write namespace using statement for cout
 
 int main()
 {
-	
+	//Variables
+	double meal_amount, tip_rate, tip_amount, tax_amount, total;
+
+	//Capture meal amount
+	std::cout << "Enter meal amount: ";
+	std::cin >> meal_amount;
+
+	//Calculate tax_amount using get_sales_tax_amount
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	//Capture tip rate
+	std::cout << "Enter tip rate as a decimal: ";
+	std::cin >> tip_rate;
+
+	//Calculate tip_amount
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+	//Calculate Total
+	total = meal_amount + tax_amount + tip_amount;
+
+	//Display
+	std::cout << "Meal Amount: " << meal_amount << std::endl;
+	std::cout << "Sales Tax: " << tax_amount << std::endl;
+	std::cout << "Tip Amount: " << tip_amount << std::endl;
+	std::cout << "Total: " << total << std::endl;
 
 	return 0;
 }
